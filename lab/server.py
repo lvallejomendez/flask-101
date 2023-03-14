10c125f7c94f0192d7d31a39a88ea53ca6cc7643
lvallejomendez <lvallejomendez@gmail.com>
2023-03-14 10:01:31 -0400

    message with make_response metohd added

--- lab/server.py
+++ lab/server.py
@@ -1,14 +1,13 @@
-from flask import Flask
+from flask import Flask, make_response
 app = Flask(__name__)
 
 # This code creates a Flask server and adds a home endpoint “/“ that returns the string hello world.
 @app.route("/")
 def index():
-    return "hello world"
+    return "Hello World!"
 
 # flask --app server --debug run
 # in another terminal run: curl -X GET -i -w '\n' localhost:5000
-
 
 # Send custom HTTP code back with a tuple.
 @app.route("/no_content")
@@ -19,3 +18,15 @@ def no_content():
         status code: 204
     """
     return ({"message":"No content found"}, 204)
+
+# Send custom HTTP code back with the make_response() method.
+@app.route("/exp")
+def index_explicit():
+    """return 'Hello World!' message with a status code of 200
+    Returns:
+        string: Hello World!
+        status code: 200
+    """
+    resp = make_response({"message":"Hello World!"})
+    resp.status_code = 200
+    return resp
